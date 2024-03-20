f87edb130ddfd7523403465137c673556ef677d7
Noi-Git <noi.patsin@gmail.com>
2024-03-20 17:52:12 -0400

    the component will have it own state eventhough it duplicates

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -3,14 +3,13 @@ import { useState } from 'react'
 const ListGroup = () => {
   const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']
 
-  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [selectedIndex, setSelectedIndex] = useState(-1)
 
   return (
     <>
       <h1>Hello List</h1>
       <ul className='list-group'>
-        <li className='list-group-item'>An item</li>
-        <li className='list-group-item'>A second item</li>
+        <li className='list-group-item text-bg-warning'>USA States</li>
         {items.map((item, index) => (
           <li
             key={index}
