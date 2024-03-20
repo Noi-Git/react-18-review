55d01228c0909e105e99ebc847e709d110538b4d
Noi-Git <noi.patsin@gmail.com>
2024-03-20 15:40:39 -0400

    display component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,13 @@
 // import { useState } from 'react'
 import './App.css'
+import ListGroup from './components/ListGroup'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  )
 }
 
 export default App
