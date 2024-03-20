2ab83c0586fbf8ec5c0ebeaca685047e2df9e572
Noi-Git <noi.patsin@gmail.com>
2024-03-20 16:30:36 -0400

    loop through an array with conditions

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,6 +2,23 @@ import { Fragment } from 'react'
 
 const ListGroup = () => {
   const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']
+  const fruits = ['Apple', 'Orange', 'Banana', 'Grape']
+  // normal way to loop through an array with out jsx expression
+  // if (fruits.length === 0)
+  //   return (
+  //     <>
+  //       <h1>List</h1>
+  //       <p>No item found</p>
+  //     </>
+  //   )
+
+  const fruitList = () => {
+    return fruits.map((fruit) => (
+      <li key={fruit} className='list-group-item text-bg-warning'>
+        {fruit}
+      </li>
+    ))
+  }
 
   return (
     <>
@@ -10,10 +27,11 @@ const ListGroup = () => {
         <li className='list-group-item'>An item</li>
         <li className='list-group-item'>A second item</li>
         {items.map((item) => (
-          <li key={item} className='list-group-item'>
+          <li key={item} className='list-group-item text-bg-success'>
             {item}
           </li>
         ))}
+        {fruits.length !== 0 ? fruitList() : 'No fruits availabel at this time'}
       </ul>
     </>
   )
