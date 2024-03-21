f52c8825e28cab9c0ae2a2108d5f09d2c8d7e01c
Noi-Git <noi.patsin@gmail.com>
2024-03-20 23:57:19 -0400

    set onClick properties to like button

--- react-app/src/components/Like.tsx
+++ react-app/src/components/Like.tsx
@@ -1,9 +1,20 @@
-import { TiHeartFullOutline } from 'react-icons/ti'
+import { useState } from 'react'
+import { TiHeartFullOutline, TiHeartOutline } from 'react-icons/ti'
 
 const Like = () => {
+  const [status, setStatus] = useState(true)
   return (
     <>
-      <TiHeartFullOutline color='#ff6b81' size={80} />
+      {status && (
+        <TiHeartFullOutline
+          color='red'
+          size={80}
+          onClick={() => setStatus(false)}
+        />
+      )}
+      {!status && (
+        <TiHeartOutline color='red' size={80} onClick={() => setStatus(true)} />
+      )}
     </>
   )
 }
