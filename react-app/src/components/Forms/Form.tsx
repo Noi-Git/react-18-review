6d03db416fa47f358994f0d4ec1b1b6f1dcd1dd1
Noi-Git <noi.patsin@gmail.com>
2024-03-22 22:14:30 -0400

    ** create handle submit function and add event type

--- react-app/src/components/Forms/Form.tsx
+++ react-app/src/components/Forms/Form.tsx
@@ -1,11 +1,13 @@
+import { FormEvent } from 'react'
+
 const Form = () => {
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    console.log('Submitted')
+  }
+
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault()
-        console.log('Submitted')
-      }}
-    >
+    <form onSubmit={handleSubmit}>
       <div className='mb-3'>
         <label htmlFor='name' className='form-label'>
           Name
