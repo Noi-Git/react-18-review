03fc5a03102dd13a995a4d713005c42837e904e5
Noi-Git <noi.patsin@gmail.com>
2024-03-28 19:04:39 -0400

    create form with useState

---
+++ react-app/src/components/Forms/FormUseState.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const FormUseState = () => {
+  const [person, setPerson] = useState({
+    name: '',
+    age: 0,
+  })
+
+  const handleSubmit = (event: FormEvent) => {
+    event?.preventDefault()
+    console.log(person.name)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className='mb-3'>
+        <label htmlFor='name' className='form-label'>
+          Name
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          id='name'
+          type='text'
+          className='form-control'
+        />
+      </div>
+      <div className='mb-3'>
+        <label htmlFor='age' className='form-label'>
+          Age
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          id='age'
+          type='number'
+          className='form-control'
+        />
+      </div>
+      <button className='btn btn-primary' type='submit'>
+        Submit
+      </button>
+    </form>
+  )
+}
+
+export default FormUseState
