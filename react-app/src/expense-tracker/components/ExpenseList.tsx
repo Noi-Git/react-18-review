e02461b11ca26ee16378eb45aa2f26e1377bfe93
Noi-Git <noi.patsin@gmail.com>
2024-03-29 13:37:23 -0400

    initialize Expense Tracker List

---
+++ react-app/src/expense-tracker/components/ExpenseList.tsx
@@ -0,0 +1,28 @@
+interface Expense {
+  id: number
+  description: string
+  amount: number
+  category: string
+}
+
+interface Props {
+  expenses: Expense[]
+}
+
+const ExpenseList = () => {
+  return (
+    <table className='table table-bordered'>
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Category</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  )
+}
+
+export default ExpenseList
