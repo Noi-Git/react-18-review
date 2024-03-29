d6f18372b831879effceead0d693045373a6dd2f
Noi-Git <noi.patsin@gmail.com>
2024-03-29 13:58:48 -0400

    create test data and use reduce function to dynamically calculate the total

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,85 +1,20 @@
-import { useState } from 'react'
 import './App.css'
-import AlertDismiss from './components/AlertDismiss'
-import Button from './components/Button'
-import Like from './components/Like'
-import ImmutableObject from './components/ImmutableObject'
-import NestedObject from './components/NestedObject'
-import ArrayUpdateWithImmer from './components/ArrayUpdateWithImmer'
-import ShareState from './components/Product/ShareState'
-import MainApp from './components/UpdateStateExercise/MainApp'
-import Pizza from './components/PizzaAddToArray/Pizza'
-import ShoppingCart from './components/ShoppingCart/ShoppingCart'
-import ExpandableText from './components/ExpandableText/ExpandableText'
-import Form from './components/Forms/Form'
-import FormUseState from './components/Forms/FormUseState'
-import FormHook from './components/Forms/FormHook'
-import FormWithZodValidation from './components/Forms/FormWithZodValidation'
+import ExpenseList from './expense-tracker/components/ExpenseList'
 
 function App() {
-  const [alertVisible, setAlertVisible] = useState(false)
+  const expenses = [
+    { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
+    { id: 2, description: 'bbb', amount: 8.5, category: 'Utilities' },
+    { id: 3, description: 'ccc', amount: 50, category: 'Utilities' },
+    { id: 4, description: 'ddd', amount: 63.29, category: 'Utilities' },
+  ]
 
   return (
     <div className='container'>
-      {alertVisible && (
-        <AlertDismiss onClose={() => setAlertVisible(false)}>
-          Hello! <strong>I am full</strong>
-        </AlertDismiss>
-      )}
-
-      <Button onClick={() => setAlertVisible(true)}>Show Alert Message</Button>
-      <br />
-      <br />
-      <Like onClick={() => console.log('Clicked!!')} />
-      <br />
-      <br />
-      <ImmutableObject />
-      <br />
-      <br />
-      <NestedObject />
-      <br />
-      <br />
-      <ArrayUpdateWithImmer />
-      <br />
-      <br />
-      <ShareState />
-      <br />
-      <br />
-      <MainApp />
-      <br />
-      <br />
-      <Pizza />
-      <br />
-      <br />
-      <ShoppingCart />
-      <br />
-      <br />
-      <ExpandableText maxChars={10}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quia
-        veritatis commodi ipsum aperiam explicabo sit officia. Repudiandae
-        suscipit soluta, quam quis harum vero commodi delectus assumenda
-        dignissimos maiores nam consequuntur ipsam ab. Corporis maiores nisi
-        placeat dolorum voluptate itaque odio! Autem, dolores ipsa. Eveniet,
-        amet doloribus? Nesciunt magni odit corporis ducimus ut deleniti
-        doloremque, porro quaerat incidunt, nam recusandae dolores blanditiis
-        praesentium officiis minima sed architecto? Quibusdam animi molestiae,
-        eligendi hic exercitationem quae repudiandae excepturi assumenda
-        reprehenderit magnam nisi deserunt et facilis. Incidunt explicabo eos
-        enim cumque dolores ullam minima nostrum nam, autem sapiente cupiditate,
-        excepturi possimus esse a.
-      </ExpandableText>
-      <br />
-      <br />
-      <Form />
-      <br />
-      <br />
-      <FormUseState />
-      <br />
-      <br />
-      <FormHook />
-      <br />
-      <br />
-      <FormWithZodValidation />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => console.log('Delete', id)}
+      />
     </div>
   )
 }
