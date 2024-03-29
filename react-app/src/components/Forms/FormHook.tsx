1ae245aa0c7cb2fd8e75f6678096b851d5b19a3a
Noi-Git <noi.patsin@gmail.com>
2024-03-28 23:21:33 -0400

    add interface

--- react-app/src/components/Forms/FormHook.tsx
+++ react-app/src/components/Forms/FormHook.tsx
@@ -1,11 +1,16 @@
 import { useForm, FieldValues } from 'react-hook-form'
 
+interface FormData {
+  name: string
+  age: number
+}
+
 const FormHook = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm<FormData>()
   console.log('formState....errors...', errors)
 
   const onsubmit = (data: FieldValues) => console.log(data)
