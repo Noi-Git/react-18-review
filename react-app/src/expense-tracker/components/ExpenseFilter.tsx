4443026b185fb1cfbe307a1489a08d38d3635247
Noi-Git <noi.patsin@gmail.com>
2024-03-29 14:23:49 -0400

    setup onChange event

--- react-app/src/expense-tracker/components/ExpenseFilter.tsx
+++ react-app/src/expense-tracker/components/ExpenseFilter.tsx
@@ -4,7 +4,10 @@ interface Props {
 
 const ExpenseFilter = ({ onSelectCategory }: Props) => {
   return (
-    <select className='form-select'>
+    <select
+      className='form-select'
+      onChange={(event) => onSelectCategory(event.target.value)}
+    >
       <option value='All categories'></option>
       <option value='Groceries'>Groceries</option>
       <option value='Utilities'>Utilities</option>
