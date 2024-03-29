504a9eefe4f9197010c1119394464b744bccc741
Noi-Git <noi.patsin@gmail.com>
2024-03-29 14:22:11 -0400

    create expense filter dropdow and interface

---
+++ react-app/src/expense-tracker/components/ExpenseFilter.tsx
@@ -0,0 +1,16 @@
+interface Props {
+  onSelectCategory: (category: string) => void
+}
+
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
+  return (
+    <select className='form-select'>
+      <option value='All categories'></option>
+      <option value='Groceries'>Groceries</option>
+      <option value='Utilities'>Utilities</option>
+      <option value='Entertainment'>Entertainment</option>
+    </select>
+  )
+}
+
+export default ExpenseFilter
