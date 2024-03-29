ea892883102e26aa4c92601221818ea62a6da815
Noi-Git <noi.patsin@gmail.com>
2024-03-28 21:57:59 -0400

    ** using Form Hook in the input field

--- react-app/src/components/Forms/FormHook.tsx
+++ react-app/src/components/Forms/FormHook.tsx
@@ -1,27 +1,32 @@
-import { FormEvent } from 'react'
 import { useForm } from 'react-hook-form'
 
 const FormHook = () => {
-  const form = useForm()
-  console.log(form)
-
-  const handleSubmit = (event: FormEvent) => {
-    event?.preventDefault()
-  }
+  const { register } = useForm()
+  console.log(register('name'))
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={() => {}}>
       <div className='mb-3'>
         <label htmlFor='name' className='form-label'>
           Name with Form Hook
         </label>
-        <input id='name' type='text' className='form-control' />
+        <input
+          {...register('name')}
+          id='name'
+          type='text'
+          className='form-control'
+        />
       </div>
       <div className='mb-3'>
         <label htmlFor='age' className='form-label'>
           Age with Form Hook
         </label>
-        <input id='age' type='number' className='form-control' />
+        <input
+          {...register('age')}
+          id='age'
+          type='number'
+          className='form-control'
+        />
       </div>
       <button className='btn btn-primary' type='submit'>
         Submit
