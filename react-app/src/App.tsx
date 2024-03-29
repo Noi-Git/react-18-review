8760d50893e103b0dd7e8716516a25300699040e
Noi-Git <noi.patsin@gmail.com>
2024-03-29 14:09:06 -0400

    use .filter to remove the item from the table

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,20 @@
+import { useState } from 'react'
 import './App.css'
 import ExpenseList from './expense-tracker/components/ExpenseList'
 
 function App() {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
     { id: 2, description: 'bbb', amount: 8.5, category: 'Utilities' },
     { id: 3, description: 'ccc', amount: 50, category: 'Utilities' },
     { id: 4, description: 'ddd', amount: 63.29, category: 'Utilities' },
-  ]
+  ])
 
   return (
     <div className='container'>
       <ExpenseList
         expenses={expenses}
-        onDelete={(id) => console.log('Delete', id)}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
   )
