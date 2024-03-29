e8a663e472503ac5dd85bb7fdccbf672d322e6cd
Noi-Git <noi.patsin@gmail.com>
2024-03-29 14:33:32 -0400

    use useState to re-render the component when category is selected

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,17 +4,21 @@ import ExpenseList from './expense-tracker/components/ExpenseList'
 import ExpenseFilter from './expense-tracker/components/ExpenseFilter'
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState('')
+
   const [expenses, setExpenses] = useState([
     { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
     { id: 2, description: 'bbb', amount: 8.5, category: 'Utilities' },
-    { id: 3, description: 'ccc', amount: 50, category: 'Utilities' },
-    { id: 4, description: 'ddd', amount: 63.29, category: 'Utilities' },
+    { id: 3, description: 'ccc', amount: 50, category: 'Entertainment' },
+    { id: 4, description: 'ddd', amount: 63.29, category: 'Groceries' },
   ])
 
   return (
     <div className='container'>
       <div className='mt-5 mb-3'>
-        <ExpenseFilter onSelectCategory={(category) => console.log(category)} />
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
       </div>
 
       <ExpenseList
